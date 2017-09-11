e74572ba3ece1177c39b240f8054977119bf5302
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-11 13:01:37 +0900

    fix create, add flash and error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,22 @@
 class ArticlesController < ApplicationController
 
+  def show
+    @article = Article.find(params[:id])
+  end
   def new
     @article = Article.new
   end
 
   def create
-    
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@articale)
+
+    if @article.save
+      flash[:notice] = "article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+
   end
 
   private
