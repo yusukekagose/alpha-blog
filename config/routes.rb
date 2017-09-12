e032b5e95b22eae5127dbd39c4da8bddbaedeb38
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-12 17:49:42 +0900

    add categories model and tests like integration and soon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root  'pages#home'
   get  'about', to: 'pages#about'
 
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
 end
