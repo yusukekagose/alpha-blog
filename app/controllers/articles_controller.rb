83828591dea6efceeb6953e72b3b31562d3270d3
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-12 13:40:16 +0900

    small update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       flash[:success] = "article was successfully created"
       redirect_to article_path(@article)
