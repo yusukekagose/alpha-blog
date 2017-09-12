e102e4e23f33b40c5f48829ddac387ef9f78480d
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-12 10:40:03 +0900

    add pagination function

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ end
 gem 'rails', '~> 5.0.6'
 # Use sqlite3 as the database for Active Record
 gem 'bcrypt', '~> 3.1.7'
+gem 'will_paginate', '3.1.0'
+gem 'bootstrap-will_paginate', '0.0.10'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:pages], per_page: 5)
   end
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   def new
     @user = User.new
