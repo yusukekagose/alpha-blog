1f3438870579f8cc0e4c9fe882a0921d9193e508
kagose yusuke <kagoseyusuke@kagose-no-MacBook-Air.local>
2017-09-12 13:38:00 +0900

    complete authentication system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
 
+  def home
+    redirect_to articles_path if logged_in?
+  end
   def index
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit,:updte,:show]
+  before_action :require_same_user, only: [:edit, :update]
   def index
     @users = User.paginate(page: params[:pages], per_page: 5)
   end
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   def new
@@ -21,12 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "update success"
       redirect_to articles_path
@@ -36,7 +34,18 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "他人の情報は編集できません"
+        redirect_to root_path
+      end
     end
 end
